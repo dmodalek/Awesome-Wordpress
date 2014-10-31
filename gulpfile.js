0f64d0e2e5b1e8a834539599b53640e6ee0c372b
Dominic Modalek <dominic@modalek.ch>
2014-10-31 22:55:08 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,14 @@ gulp.task('bower', function() {
         .pipe(gulp.dest('public'))
 	gulp.src(['bower_components/awesome-theme/**'])
         .pipe(gulp.dest('public/wp-content/themes/awesome-theme'))
-    gulp.src(['bower_components/advanced-custom-fields/**'])
+
+	gulp.src(['bower_components/advanced-custom-fields/**'])
         .pipe(gulp.dest('public/wp-content/plugins/advanced-custom-fields'))
+	gulp.src(['bower_components/force-regenerate-thumbnails/**'])
+        .pipe(gulp.dest('public/wp-content/plugins/force-regenerate-thumbnails'))
+	gulp.src(['bower_components/contact-form-7/**'])
+        .pipe(gulp.dest('public/wp-content/plugins/contact-form-7'))
+	gulp.src(['bower_components/really-simple-captcha/**'])
+        .pipe(gulp.dest('public/wp-content/plugins/really-simple-captcha'))
 });
 
